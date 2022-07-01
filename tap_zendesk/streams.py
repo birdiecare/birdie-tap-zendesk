d0c52a460b28524692504f420ed17d91338694f1
benwhite-birdiecare <ben.white@birdie.care>
2022-07-01 13:54:53 +0100

    Disable incremental load for org memberships as there's no mechanism for detecting deleted memberships.

--- tap_zendesk/streams.py
+++ tap_zendesk/streams.py
@@ -163,29 +163,11 @@ class Organizations(Stream):
 
 class OrganizationMemberships(Stream):
     name = "organization_memberships"
-    replication_method = "INCREMENTAL"
-    replication_key = "updated_at"
+    replication_method = "FULL_TABLE"
 
-    def sync(self, state):
-        bookmark = self.get_bookmark(state)
-
-        memberships = self.client.organization_memberships()
-        for membership in memberships:
-            # some organization memberships come back without an updated_at
-            if membership.updated_at:
-                if utils.strptime_with_tz(membership.updated_at) >= bookmark:
-                    # NB: We don't trust that the records come back ordered by
-                    # updated_at (we've observed out-of-order records),
-                    # so we can't save state until we've seen all records
-                    self.update_bookmark(state, membership.updated_at)
-                    yield (self.stream, membership)
-            else:
-                if membership.id:
-                    LOGGER.info('organization_membership record with id: ' + str(membership.id) +
-                                ' does not have an updated_at field so it will be syncd...')
-                    yield (self.stream, membership)
-                else:
-                    LOGGER.info('Received organization_membership record with no id or updated_at, skipping...')
+    def sync(self, state): # pylint: disable=unused-argument
+        for organization_membership in self.client.organization_memberships():
+            yield (self.stream, organization_membership)
 
 class Users(Stream):
     name = "users"
